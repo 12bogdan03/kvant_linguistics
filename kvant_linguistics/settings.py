62adb7a6f4e7a00821d8e717feb1ae3045b995a8
12bogdan03 <12bogdan03@gmail.com>
2017-11-29 16:45:47 +0200

    fixes

--- kvant_linguistics/settings.py
+++ kvant_linguistics/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = '*-u!&^a(dz_jz(n4fagaa!@c%sxyysc(5x8m6ser@#8p%uc@s+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
